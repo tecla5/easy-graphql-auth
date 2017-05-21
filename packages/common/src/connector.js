714253dc51902e689d485dea6b24102c6ee29fa6
Kristian Mandrup <kmandrup@gmail.com>
2017-05-21 08:35:02 +0200

    add connector validate and skeleton test

--- packages/common/src/connector.js
+++ packages/common/src/connector.js
@@ -1,25 +1,25 @@
 module.exports = class BaseGCAuth0Connector {
   constructor(config = {}) {
-    if (typeof config !== 'object') {
-      throw Error('config must be an object')
-    }
-
+    this.validate(config)
     this.config = config
     this.store = config.store
-
-    if (!config.storage) {
-      throw Error('missing storage config')
-    }
     this.storage = config.storage
     this.keyNames = config.storage // alias
-
-    if (!config.graphCool) {
-      throw Error('missing graphCool config')
-    }
-
     this.auth0 = config.auth0
     this.graphCool = config.graphCool
     this.tokens = this.store ? this.store.getAll() : config.tokens
     this.connection = config.graphCool.connection
+  }
+
+  validate() {
+    if (!config.storage) {
+      throw Error('missing storage config')
+    }
+    if (!config.graphCool) {
+      throw Error('missing graphCool config')
+    }
+    if (typeof config !== 'object') {
+      throw Error('config must be an object')
+    }
   }
 }
