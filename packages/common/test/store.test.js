0c3a75b51096e8afc9e8183734710e3e22a10b35
Kristian Mandrup <kmandrup@gmail.com>
2017-05-22 17:03:49 +0200

    merged ok

--- packages/common/test/store.test.js
+++
@@ -1,6 +0,0 @@
-import test from 'ava'
-import Store from '../src/store'
-
-test('Store', t => {
-  t.is(Store, 'function')
-})
--- packages/lokka/src/index.js
+++ packages/lokka/src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import {
   setup,
   Store,
@@ -6,6 +7,16 @@ import {
   createLock,
   jwtUtil
 } from '@tecla5/gc-auth0-common'
+=======
+// const {
+//   setup,
+//   Store,
+//   createStore,
+//   Lock,
+//   createLock,
+//   jwtUtil
+// } = require('@tecla5/gc-auth-common')
+>>>>>>> Stashed changes
 
 import GCAuth0Connector from './connector'
 import createClient from './create-client'
@@ -14,10 +25,10 @@ export default {
   GCAuth0Connector,
   createClient,
   // from gc-auth0-common
-  Lock,
-  createLock,
-  jwtUtil,
-  Store,
-  createStore,
-  setup
+  // Lock,
+  // createLock,
+  // jwtUtil,
+  // Store,
+  // createStore,
+  // setup
 }
